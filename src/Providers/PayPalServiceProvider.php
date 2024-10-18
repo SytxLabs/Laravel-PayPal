ee242e2cc726bb126c21d55aaf817fa859fe44a2
Shaun Lüdeke <77498048+shaunluedeke@users.noreply.github.com>
2024-10-18 09:15:06 +0200

    Add conditional check for publishesMigrations method
    Ensure compatibility with Laravel versions where publishesMigrations method does not exist. Fall back to using publishes method if publishesMigrations is not available.

--- src/Providers/PayPalServiceProvider.php
+++ src/Providers/PayPalServiceProvider.php
@@ -16,10 +16,15 @@ class PayPalServiceProvider extends ServiceProvider
                 __DIR__ . '/../../config/paypal.php' => config_path('paypal.php'),
             ], 'sytxlabs-paypal-config');
 
-            $this->publishesMigrations([
-                __DIR__ . '/../../database/migrations' => database_path('migrations'),
-            ], 'sytxlabs-paypal-migrations');
-
+            if (method_exists($this, 'publishesMigrations')) {
+                $this->publishesMigrations([
+                    __DIR__ . '/../../database/migrations' => database_path('migrations'),
+                ], 'sytxlabs-paypal-migrations');
+            } else {
+                $this->publishes([
+                    __DIR__ . '/../../database/migrations' => database_path('migrations'),
+                ], 'sytxlabs-paypal-migrations');
+            }
             AboutCommand::add('SytxLabs Laravel Paypal Package', static fn () => ['Version' => '1.0.0', 'Author' => 'SytxLabs']);
         }
     }
