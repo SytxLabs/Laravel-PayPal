4fda0f2fff3ea1546a7d3771bf0cf50e08b024cb
Shaun Lüdeke <77498048+shaunluedeke@users.noreply.github.com>
2024-10-18 12:48:06 +0200

    Validate email format in Payee constructor
    Added a regular expression check to validate email addresses in the Payee constructor. An InvalidArgumentException is thrown if the email format is invalid. This ensures that only properly formatted email addresses are accepted.

--- src/Models/Payee.php
+++ src/Models/Payee.php
@@ -3,6 +3,7 @@
 namespace SytxLabs\PayPal\Models;
 
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 
 class Payee extends \PaypalServerSDKLib\Models\Payee
 {
@@ -12,7 +13,10 @@ class Payee extends \PaypalServerSDKLib\Models\Payee
     public function __construct(?string $email, ?string $merchantId = null, ?string $referenceId = null, ?PayeeShippingDetail $shippingDetail = null)
     {
         if ($email !== null && preg_match('/^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/', $email) !== 1) {
-            throw new \InvalidArgumentException('Invalid email address');
+            throw new InvalidArgumentException('Invalid email address');
+        }
+        if ($merchantId !== null && preg_match('/^[2-9A-HJ-NP-Z]/', $merchantId) !== 1) {
+            throw new InvalidArgumentException('Invalid merchant ID');
         }
         $this->setEmailAddress($email);
         $this->setMerchantId($merchantId);
